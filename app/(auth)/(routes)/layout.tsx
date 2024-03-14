6a715f7d5f140d3400dd4ef94eb640f0eec64ce2
agt1984 <69942981+agt1984@users.noreply.github.com>
2024-03-14 14:19:39 +0100

    basic auth configuration added

---
+++ app/(auth)/(routes)/layout.tsx
@@ -0,0 +1,12 @@
+//aspecto del auth
+export default function AuthLayout({
+    children 
+}: {
+    children: React.ReactNode
+}) {
+    return (
+        <div className="flex items-center justify-center h-full">
+            {children}
+        </div>
+    )
+}
